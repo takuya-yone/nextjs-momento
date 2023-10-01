eb50c2381f0c5f76008081048998fae4e4b61158
Takuya Yonezawa <yonezawa.takuya.yq2@gmail.com>
2023-10-02 04:27:20 +0900

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,115 +1,293 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect } from "react";
+
+import React, { useEffect, useState, useRef, cache } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { Tag } from "@chakra-ui/tag";
+import { Input, InputGroup, InputRightElement } from "@chakra-ui/input";
+import { FormControl } from "@chakra-ui/form-control";
+import { Button } from "@chakra-ui/button";
+
+import {
+  Configurations,
+  CredentialProvider,
+  MomentoErrorCode,
+  TopicClient,
+  type TopicItem,
+  TopicPublish,
+  TopicSubscribe,
+} from "@gomomento/sdk-web";
+
+enum EventTypes {
+  MESSAGE = "message",
+  USER_JOINED = "user_joined",
+}
+
+type ChatMessageEvent = {
+  event: EventTypes.MESSAGE;
+  username: string;
+  text: string;
+  // timestamp: number;
+};
+
+type UserJoinedEvent = {
+  event: EventTypes.USER_JOINED;
+  username: string;
+  // timestamp: number;
+};
+
+type ChatEvent = UserJoinedEvent | ChatMessageEvent;
+
+let webTopicClient: TopicClient | undefined = undefined;
+let subscription: TopicSubscribe.Subscription | undefined = undefined;
+let onItemCb: (item: TopicItem) => void;
+let onErrorCb: (
+  error: TopicSubscribe.Error,
+  subscription: TopicSubscribe.Subscription
+) => Promise<void>;
+
+type MomentoClients = {
+  topicClient: TopicClient;
+};
+
+async function getNewWebClients(): Promise<MomentoClients> {
+  webTopicClient = undefined;
+  // we don't want to cache the token, since it will expire in 5 min
+  // const fetchResp = await fetch(window.location.origin + "/api/momento/token", {
+  //   cache: "no-store",
+  // });
+  // const token = await fetchResp.text();
+  const topicClient = new TopicClient({
+    configuration: Configurations.Browser.v1(),
+    credentialProvider: CredentialProvider.fromString({
+      apiKey: String(process.env.NEXT_PUBLIC_MOMENTO_API_KEY),
+    }),
+  });
+  webTopicClient = topicClient;
+  return {
+    topicClient,
+  };
+}
+
+const clearCurrentClient = () => {
+  subscription?.unsubscribe();
+  subscription = undefined;
+  webTopicClient = undefined;
+};
+
+async function getWebTopicClient(): Promise<TopicClient> {
+  if (webTopicClient) {
+    return webTopicClient;
+  }
+
+  const clients = await getNewWebClients();
+  return clients.topicClient;
+}
+
+// export async function listCaches(): Promise<string[]> {
+//   const fetchResp = await fetch(window.location.origin + "/api/momento/caches");
+//   const caches: string[] = await fetchResp.json();
+//   return caches;
+// }
+
+async function subscribeToTopic(
+  cacheName: string,
+  topicName: string,
+  onItem: (item: TopicItem) => void,
+  onError: (
+    error: TopicSubscribe.Error,
+    subscription: TopicSubscribe.Subscription
+  ) => Promise<void>
+) {
+  onErrorCb = onError;
+  onItemCb = onItem;
+  const topicClient = await getWebTopicClient();
+  const resp = await topicClient.subscribe(cacheName, topicName, {
+    onItem: onItemCb,
+    onError: onErrorCb,
+  });
+  if (resp instanceof TopicSubscribe.Subscription) {
+    subscription = resp;
+    return subscription;
+  }
+
+  throw new Error(`unable to subscribe to topic: ${resp}`);
+}
+
+async function publish(cacheName: string, topicName: string, message: string) {
+  const topicClient = await getWebTopicClient();
+  const resp = await topicClient.publish(cacheName, topicName, message);
+  if (resp instanceof TopicPublish.Error) {
+    if (resp.errorCode() === MomentoErrorCode.AUTHENTICATION_ERROR) {
+      console.log(
+        "token has expired, going to refresh subscription and retry publish"
+      );
+      clearCurrentClient();
+      await subscribeToTopic(cacheName, topicName, onItemCb, onErrorCb);
+      await publish(cacheName, topicName, message);
+    } else {
+      console.error("failed to publish to topic", resp);
+    }
+  }
+}
+
+async function userJoined(
+  cacheName: string,
+  topicName: string,
+  username: string
+) {
+  const userJoinedEvent: UserJoinedEvent = {
+    username,
+    // timestamp: Date.now(),
+    event: EventTypes.USER_JOINED,
+  };
+  await publish(cacheName, topicName, JSON.stringify(userJoinedEvent));
+}
+
+async function sendMessage(
+  cacheName: string,
+  topicName: string,
+  username: string,
+  text: string
+) {
+  const chatMessage: ChatMessageEvent = {
+    username,
+    text,
+    // timestamp: Date.now(),
+    event: EventTypes.MESSAGE,
+  };
+  await publish(cacheName, topicName, JSON.stringify(chatMessage));
+}
+
+type Props = {
+  topicName: string;
+  cacheName: string;
+  username: string;
+  onLeave: () => void;
+};
+
+function ChatInput(props: {
+  cacheName: string;
+  topicName: string;
+  userName: string;
+}) {
+  const [input, setInput] = useState("");
+  const handleInputChange = (e: any) => setInput(e.target.value);
+
+  const handleChatInputSubmit = () => {
+    sendMessage(props.cacheName, props.topicName, props.userName, input);
+    setInput("");
+  };
+
+  useEffect(() => {}, [input]);
+
+  return (
+    <FormControl className="flex  flex-col items-center m-1">
+      <InputGroup size="md" width={400}>
+        <Input
+          pr="4.5rem"
+          type="text"
+          placeholder="Enter Chat"
+          value={input}
+          onChange={handleInputChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleChatInputSubmit();
+            }
+          }}
+        />
+        <InputRightElement width="4.5rem">
+          <Button
+            colorScheme="teal"
+            variant="solid"
+            h="1.75rem"
+            size="sm"
+            type="submit"
+            onClick={handleChatInputSubmit}
+          >
+            {"SEND"}
+          </Button>
+        </InputRightElement>
+      </InputGroup>
+    </FormControl>
+  );
+}
+
+function ChatToolTip(props: { chat: ChatEvent }) {
+  if ("text" in props.chat) {
+    return (
+      <Tag className="m-1" key={uuidv4()} colorScheme="blue">
+        {props.chat.text}
+      </Tag>
+    );
+  } else {
+    return (
+      <Tag className="m-1" key={uuidv4()} colorScheme="pink">
+        {`${props.chat.username} has joinded`}
+      </Tag>
+    );
+  }
+}
 
 export default function Home() {
+  // const userName = uuidv4();
+  const [userName, setUserName] = useState<string>("");
+  const cacheName = String(process.env.NEXT_PUBLIC_MOMENTO_CACHE_NAME);
+  const topicName = String(process.env.NEXT_PUBLIC_MOMENTO_TOPIC_NAME);
+
+  const [chats, setChats] = useState<ChatEvent[]>([]);
+
+  const onItem = (item: TopicItem) => {
+    try {
+      const message = JSON.parse(item.valueString()) as ChatEvent;
+      // console.log(typeof message);
+      setChats((curr) => [...curr, message]);
+    } catch (e) {
+      console.error("unable to parse chat message", e);
+    }
+  };
+
+  const onError = async (
+    error: TopicSubscribe.Error,
+    sub: TopicSubscribe.Subscription
+  ) => {
+    console.error(
+      "received error from momento, getting new token and resubscribing",
+      error
+    );
+    sub.unsubscribe();
+    clearCurrentClient();
+    await subscribeToTopic(cacheName, topicName, onItem, onError);
+  };
+
+  useEffect(() => {
+    const name = uuidv4();
+    setUserName(name);
+    subscribeToTopic(cacheName, topicName, onItem, onError)
+      .then(() => {
+        console.log("successfully subscribed");
+        // sendMessage(cacheName, topicName, "aiueo", "message");
+        userJoined(cacheName, topicName, name);
+      })
+      .catch((e) => {
+        console.error("error subscribing to topic", e);
+      });
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main className="flex min-h-screen flex-col items-center  p-24">
+      <Tag className="m-1" size="lg" key={uuidv4()} colorScheme="green">
+        {userName}
+      </Tag>
+      <ChatInput
+        cacheName={cacheName}
+        topicName={topicName}
+        userName={userName}
+      />
+      <p className="m-1">↓Chat↓</p>
+      {chats.map((chat) => (
+        <ChatToolTip key={uuidv4()} chat={chat} />
+      ))}
     </main>
   );
 }
